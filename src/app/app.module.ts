61dc98aa67aa059f62027f81d9aafff58b037c19
Pavel_Kazakov <Pavel_Kazakov@epam.com>
2018-03-21 17:06:34 +0400

    add reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CKEditorModule } from 'ng2-ckeditor';
 
 import { AppComponent } from './app.component';
+import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditTodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/edit-todo/edit-todo.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'pk-edit-todo',
+  templateUrl: './edit-todo.component.html',
+  styleUrls: ['./edit-todo.component.css']
+})
+export class EditTodoComponent implements OnInit {
+  private todoForm: FormGroup;
+  private data: Observable<any>;
+  private ckeditorContent: string = '<p>Some html</p>';
+  private selectedTodo: any;
+  private todos: any = [
+    {id: 'todo1', description: 'description1'},
+    {id: 'todo2', description: 'description2'},
+    {id: 'todo3', description: 'description3'},
+    {id: 'todo4', description: 'description4'}
+  ];
+  
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.todoForm = this.formBuilder.group({
+      id: ['', Validators.required],
+      description: ''
+    });
+
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(this.todos);
+      }, 1000);
+    });
+
+    let subscription = this.data.subscribe( value => {
+      this.selectedTodo = value[0];
+      this.selectTodo(this.selectedTodo);
+    });
+
+    this.todoForm.valueChanges.subscribe( form => {
+      for (let key in form) {
+        this.selectedTodo[key] = form[key];
+      }
+    });
+  }
+
+  selectTodo(todo) {
+    this.selectedTodo = todo;
+    this.todoForm.patchValue({
+      id: todo.id || '',
+      description: todo.description || ''
+    });
+  }
+}
